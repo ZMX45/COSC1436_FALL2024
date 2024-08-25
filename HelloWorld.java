d5355df38e89127e2c10effb5247e6d1e50e799b
ZMX45 <zmart99@yahoo.com>
2024-08-25 21:02:25 +0000

    #second commit

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld
+// This line is used to create a class
+{
+    public static void main(String [] args)
+    // This line is used to initiate the program
+    {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Zach.");
+        System.out.println("My favorite food is barbacoa tacos.");
+        System.out.println("I would like to meet Quentin Tarantino.");
+        System.out.println("My least favorite chore is washing the dishes.");
+        System.out.println("Right now I'd rather be playing games with friends.");
+        /* Code comments are used to let the writer explain and organize their code.
+         * System.out.println is used to tell the system to output a line of text graphically.
+         * main() method is used as a starting point for the program.
+         */
+    }
+}
